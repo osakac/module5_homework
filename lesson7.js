570e2b6311263f58f1f24424eae9b83204e4a4b0
osakac <shinmoriof@gmail.com>
2022-11-17 12:29:30 +0400

    update lesson

--- lesson7.js
+++ lesson7.js
@@ -1,10 +1,10 @@
-const arr = [3, 42, null, 35, 'string', 99, false, 66]
+const arr = [3, 42, null, 35, 'string', 99, false, 66, NaN]
 
 let even = 0
 let odd = 0
 
 arr.forEach(el => {
-  if (typeof el === 'number') {
+  if (typeof el === 'number' && !isNaN(el)) {
     if (el % 2 === 0) {
       even += 1
     } else {
